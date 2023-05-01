beaa3a3207dcec2e39d13882d17b8b72e11becb2
fadelalif <fadelkarut@gmail.com>
2023-05-01 23:47:42 +0700

    menambahkan libraries dan driver

---
+++ AljabarLibraries/PersamaanKuadrat.cs
@@ -0,0 +1,41 @@
+﻿namespace AljabarLibraries
+{
+    public class PersamaanKuadrat
+    {
+        public double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+            double diskriminan = (b * b) - (4 * a * c);
+
+            if (diskriminan < 0)
+            {
+                return new double[] { };
+            }
+            else if (diskriminan == 0)
+            {
+                double x = -b / (2 * a);
+                return new double[] { x };
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+                return new double[] { x1, x2 };
+            }
+        }
+
+        public double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double y1 = a * a;
+            double y2 = 2 * a * b;
+            double y3 = b * b;
+
+            return new double[] { y1, y2, y3 };
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,26 @@
+﻿using AljabarLibraries;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double[] persamaan1 = { 1, -3, -10 };
+        double[] persamaan2 = { 2, -3 };
+        PersamaanKuadrat persamaanKuadrat = new PersamaanKuadrat();
+
+        double[] hasilAkar = persamaanKuadrat.AkarPersamaanKuadrat(persamaan1);
+        double[] hasilKuadrat = persamaanKuadrat.HasilKuadrat(persamaan2);
+
+        Console.WriteLine("Hasil akar persamaan kuadrat:");
+        foreach (double akar in hasilAkar)
+        {
+            Console.Write(akar + " ");
+        }
+
+        Console.WriteLine("\nHasil kuadrat:");
+        foreach (double hasil in hasilKuadrat)
+        {
+            Console.Write(hasil + " ");
+        }
+
+    }
+}
